3bd403f1065ec0b1b7d7637f7157344b6ed8f545
Paulo Lopes <paulo.lopes@bit.admin.ch>
2018-04-06 09:31:30 +0200

    chek if maven and java is install

---
+++ desktop_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# ansible-playbook -i hosts playbook.yml -c ssh --ask-pass -u dev -become-method=sudo   --extra-vars "ansible_sudo_pass=secret"
+# ansible-playbook playbook.yml --connection=local --extra-vars "ansible_sudo_pass=secret"
+
+ echo "Install Jeap-Ansible"
+
+ ansible-playbook playbook.yml --connection=local --ask-pass -u dev -become-method=sudo   --extra-vars "ansible_sudo_pass=secret"

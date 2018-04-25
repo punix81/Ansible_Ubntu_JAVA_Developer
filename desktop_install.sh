029890dd285c661c7d4764ad658d3c8c7c819874
Paulo Lopes <paulo.lopes@bit.admin.ch>
2018-04-25 16:19:09 +0200

    add meta for galaxy and refactoring

--- desktop_install.sh
+++ desktop_install.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
-# ansible-playbook -i hosts playbook.yml -c ssh --ask-pass -u dev -become-method=sudo   --extra-vars "ansible_sudo_pass=secret"
-# ansible-playbook playbook.yml --connection=local --extra-vars "ansible_sudo_pass=secret"
 
- echo "Install Jeap-Ansible"
+if [ -e ~/.gitconfig ]
+then
+    git fetch
+    git pull
+else
+    # Git Configuration
+    echo '###Congigure Git..'
 
+    echo "Enter the Global Username for Git:";
+    read GITUSER;
+    git config --global user.name "${GITUSER}"
+
+    echo "Enter the Global Email for Git:";
+    read GITEMAIL;
+    git config --global user.email "${GITEMAIL}"
+
+    echo '###Git has been configured!'
+    git config --list
+
+
+    echo '###Update Project'
+    git fetch
+    git pull
+fi
+
+ echo "###Init to install your desktop"
  ansible-playbook playbook.yml --connection=local -become-method=sudo   --extra-vars "ansible_sudo_pass=secret"

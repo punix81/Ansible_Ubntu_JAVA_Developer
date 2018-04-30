c0818d57466e5444003d5edde7bffb90d46138f5
Paulo Lopes <paulo.lopes@bit.admin.ch>
2018-04-30 17:19:13 +0200

    add missing licence

---
+++ desktop_updates.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+if [ -e ~/.gitconfig ]
+then
+    git fetch
+    git pull
+else
+    # Git Configuration
+    echo '###Congigure Git..'
+
+    echo "Enter the Global Username for Git:";
+    read GITUSER;
+    git config --global user.name "${GITUSER}"
+
+    echo "Enter the Global Email for Git:";
+    read GITEMAIL;
+    git config --global user.email "${GITEMAIL}"
+
+    echo "###Set git configuration";
+
+    git config --global url.https://.insteadOf git://
+
+    git config --global http.proxy http://proxy-bvcol.admin.ch:8080
+
+    git config --global http.sslVerify false
+
+    git config --global https.proxy http://proxy-bvcol.admin.ch:8080
+
+    git config --global http.http://stash.eap.bit.admin.ch.proxy ""
+
+    git config --global http.https://stash.eap.bit.admin.ch.proxy ""
+
+    echo '###Git has been configured!'
+    git config --list
+
+
+    echo '###Update Project'
+    git fetch
+    git pull
+fi
+
+
+
+if [ -e ~/.npmrc ]
+then
+ rm -rf ~/.npmrc
+fi
+
+if [ -e ~/.curlrc ]
+then
+ rm -rf ~/.curlrc
+fi
+
+if [ -e ~/.curlrc ]
+then
+ rm -rf ~/.curlrc
+fi
+
+
+echo "###Remove Intellij"
+rm -rf ~/.idea/
+
+echo "###Remove Intellij"
+rm -rf ~/development/ide/
+
+echo "###Remove java"
+rm -rf ~/development/java/
+
+echo "###Remove kitematic"
+rm -rf ~/development/kitematic/
+
+echo "###Remove maven"
+rm -rf ~/development/maven/
+
+echo "###Remove sqlDeveloper"
+rm -rf ~/development/sqlDeveloper/
+
+echo "###Remove tomcat"
+rm -rf ~/development/tomcat/
+
+
+ echo "###ReInit to install your desktop"
+ ansible-playbook playbook.yml --connection=local -become-method=sudo   --extra-vars "ansible_sudo_pass=secret"
